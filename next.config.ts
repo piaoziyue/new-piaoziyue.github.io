2b73650b64d434ae03c3cad741fdcfedd8c4c151
piaoziyue <ziyue.piao@mail.mcgill.ca>
2025-01-26 16:59:21 -0500

    update next.config.ts setting for website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  output: "export", // Necessary for exporting a static site
+  basePath: isProd ? "/piaoziyue.github.io" : "", // Update with your repository name
+  assetPrefix: isProd ? "/piaoziyue.github.io/" : "", // Ensures assets have the correct path in production
   images: {
+    unoptimized: true, // Required for static site export to handle images
     remotePatterns: [
       {
         protocol: "https",
