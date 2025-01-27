0d92d798387a2acf8306c9e7dc37e013effc6836
piaoziyue <ziyue.piao@mail.mcgill.ca>
2025-01-26 23:13:27 -0500

    update the space between news

--- src/components/news-entry.tsx
+++ src/components/news-entry.tsx
@@ -3,10 +3,10 @@ import { News } from "@/data/news";
 
 export function NewsEntry({ news }: { news: News }) {
   return (
-    <div className="flex flex-row gap-6">
+    <div className="flex flex-row gap-4">
       <div className="flex flex-col flex-1">
-        <p className="text-xs text-zinc-500 mb-2">{news.date}</p>
-        <h3 className="font-serif text-md mb-3">
+        <p className="text-xs text-zinc-500 md-1">{news.date}</p>
+        <h4 className="font-serif text-md">
           {news.link ? (
             <a
               href={news.link}
@@ -23,8 +23,7 @@ export function NewsEntry({ news }: { news: News }) {
           ) : (
             news.title
           )}
-        </h3>
-        {/* <p className="text-sm text-zinc-600">{news.description}</p> */}
+        </h4>
       </div>
     </div>
   );
